dfb8404b85425bd66dcb93a8b008d4abc03b475c
nikhilsuryavanshi0005 <nikhilsuryavanshi0005@gmail.com>
2025-10-05 23:40:51 +0530

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int Array[7];
+    Array[0] = 11;
+    Array[1] = 21;
+    Array[2] = 31;
+    Array[3] = 41;
+    Array[4] = 51;
+    Array[5] = 61;
+    Array[6] = 71;
+    printf("The Array List is :%d\n",Array);
+    printf("The Sizeof Array is :%d\n",sizeof(Array));
+    return 0;
+}
