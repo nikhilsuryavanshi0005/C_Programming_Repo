aaab76612d6c8aee8efc4a85f6d06a4251080e6a
nikhilsuryavanshi0005 <nikhilsuryavanshi0005@gmail.com>
2025-11-19 00:32:31 +0530

    Add files via upload

---
+++ headertest.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+//Entry point function;
+
+#define MAX 100
+
+int main()
+{
+    int No = MAX + 11;
+
+    printf("Value of No is :%d",No);
+
+
+
+    
+    return 0;
+}
---
+++ helper.c
@@ -0,0 +1,9 @@
+#include "marvellous.h"
+
+float CircleArea(float fRadius)
+{
+    float Area = 0.0f;
+    Area = PI *fRadius* fRadius;
+
+    return Area;
+}
